2348e96b4dcdc59cc9085bc70398f8d8262157b8
Danny <dannybanko@gmail.com>
2021-06-03 15:03:01 -0700

    all html pages restyled

--- static/scripts/customer_navigation_template.js
+++ static/scripts/customer_navigation_template.js
@@ -2,15 +2,20 @@
  * Creates a custom NavigationBar component. Can be called as a tag using:
  * <customer-nav-bar></customer-nav-bar>
  */
-class CustomerNavigationBar extends HTMLElement {
+ class CustomerNavigationBar extends HTMLElement {
   /** Sets the HTML for the NavigationBar element. */
   connectedCallback() {
-    this.innerHTML = `
+      this.innerHTML = `
+      <nav>
+      <div class="logo">
+        <p>Database 2: The SQL</p>
+      </div>
       <ul id="nav-container">
         <li class="link-container"><a class="link" href="/">Home</a></li>
         <li class="link-container"><a class="link" href="/rate">Rate Rental</a></li>
         <li class="link-container"><a class="link" href="/login">Agent Portal</a></li>
-      </ul>`.trim();
+      </ul>
+      </nav>`.trim();
   }
 }
 
